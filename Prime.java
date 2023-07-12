1267f041759fc4b7bb467a5035d42ac21dd6aacb
adityait0801 <139343673+adityait0801@users.noreply.github.com>
2023-07-12 16:11:07 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Prime {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Size of an array");
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Target Number");
+    int t = sc.nextInt();
+    System.out.println("Numbers are");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+    for (int i = 0; i <= n; i++) {
+      int count = 0;
+      for (int j = 1; j <=arr[i]; j++) {
+        int a = 0;
+        for (int k = 1; k < t; k++) {
+          int a = i + k;
+          if (a % j == 0) {
+            count++;
+          }
+          if (count == 2) {
+            System.out.println("Prime Numbers are");
+            System.out.println(a);
+          }
+        }
+      }
+    }
+  }
+}
